469f6bd94f568ff2f065fe29455aa42afd4e975e
Ruiner0 <91546635+Ruiner0@users.noreply.github.com>
2023-02-01 14:50:15 +0530

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let header = document.getElementById("main-header");
-let item = document.getElementById("addItems");
+let items = document.getElementsByClassName("list-group-item");
 
 header.style.borderBottom = "solid 3px #000";
-item.style.fontWeight="bold";
-item.style.color="green"
+
+items[2].style.backgroundColor='green';
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold';
+}
