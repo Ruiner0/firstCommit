f361c81e47d4ee5197d70f980e7e4a8077b60472
Ruiner0 <91546635+Ruiner0@users.noreply.github.com>
2023-02-01 23:58:13 +0530

    Filter

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let form=document.getElementById('addForm');
 let itemList=document.getElementById('items');
+let filter=document.getElementById('filter');
 
 form.addEventListener('submit',addItems);
 itemList.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterItems);
 
 function addItems(e){
     e.preventDefault();
 
     let newItem=document.getElementById('item').value;
+    let newItem1=document.getElementById('item1').value;
     let li=document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(newItem))
+    li.appendChild(document.createTextNode(" "+newItem1))
 
     let delBtn=document.createElement('button');
     delBtn.className='btn btn-danger btn-sm float-right delete';
@@ -29,3 +33,19 @@ function removeItem(e){
         }
     }
 }
+
+function filterItems(e){
+    let text=e.target.value.toLowerCase();
+    let items=itemList.getElementsByTagName('li');
+    
+
+    Array.from(items).forEach(function(item){
+        let itemName=item.firstChild.textContent;
+        if(itemName.toLocaleLowerCase().indexOf(text) != -1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    })
+}
+
