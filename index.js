70c16fc264fb3780fd3d732a54beab7e24d9ec65
Ruiner0 <91546635+Ruiner0@users.noreply.github.com>
2023-02-01 14:21:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const btn=document.querySelector(".btn")
-const n=document.querySelector("#name")
-const em=document.querySelector("#email")
+let header = document.getElementById("main-header");
+let item = document.getElementById("addItems");
 
-my_form.addEventListener('submit', onSubmit)
-
-// btn.addEventListener('click', (e)=>{
-//     e.preventDefault();
-//     console.log('click')
-// })
-
-btn.addEventListener('mouseover', (e)=>{
-    e.preventDefault();
-    console.log('mouseover')
-})
-
-btn.addEventListener('mouseout', (e)=>{
-    e.preventDefault();
-    console.log('mouseout')
-})
-
-function onSubmit(e){
-    e.preventDefault();
-
-    const li=document.createElement('li');
-    li.appendChild(document.createTextNode(`${n.value}:${em.value}`))
-    console.log(li)
-
-    n.value=''
-    em.value=''
-}
+header.style.borderBottom = "solid 3px #000";
+item.style.fontWeight="bold";
+item.style.color="green"
