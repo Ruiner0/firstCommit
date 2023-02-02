a50b2355607a272eb4744e0130581ae40e73abdf
Ruiner0 <91546635+Ruiner0@users.noreply.github.com>
2023-02-02 13:36:22 +0530

    Lets Scale the app to more users

--- form.js
+++ form.js
@@ -1,13 +1,19 @@
+let ulist=document.getElementById('ul')
+let li=document.createElement('li')
+li.className='ul-item'
 
 document.getElementById('my_form').addEventListener('submit', disp);
+
 function disp(e){
     e.preventDefault();
 
     let n=e.target.name;
     let email=e.target.email;
-
     
     localStorage.setItem('username',n.value)
     localStorage.setItem('email',email.value)
+
+    li.appendChild(document.createTextNode("Username : "+n.value+", Email : "+email.value))
+    ulist.appendChild(li)
 }
 
